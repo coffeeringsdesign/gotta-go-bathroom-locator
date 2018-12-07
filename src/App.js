0c690a26f90a94fe4457dec5d75dbc641e4b5314
Kendra <kendra@coffeeringsdesign.com>
2018-12-07 10:28:52 -0800

    add in map component and begin to style page

--- src/App.js
+++ src/App.js
@@ -3,18 +3,24 @@ import './App.scss';
 import BathroomList from './components/BathroomList';
 import SearchBar from './components/SearchBar';
 import Logo from './components/Logo';
+import Map from './components/Map';
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Logo />
-        <SearchBar />
-        <BathroomList />
+        <div>
+          <Logo />
+          <SearchBar />
+          <BathroomList />
+        </div>
+        <div>
+          <Map />
+        </div>
       </div>
     );
   }
-}
+};
 
 export default App;
---
+++ src/components/Map.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.scss';
+
+function Map() {
+  return (
+    <div className="mapContainer">
+      <h6> the map goes here</h6>
+    </div>
+  )
+};
+
+export default Map;
