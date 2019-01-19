e87110344d55652e773bf5d23de231968f62fd22
Kendra <kendra@coffeeringsdesign.com>
2019-01-19 10:48:23 -0800

    work in all bathroom info

--- src/actions/index.js
+++ src/actions/index.js
@@ -9,6 +9,15 @@ export function fetchDistanceDuration(indivBathroomInfo, props) {
   return function (dispatch) {
 
     const localLocation = v4();
+    const bathName = indivBathroomInfo.name;
+    const bathAddress = indivBathroomInfo.address;
+    const bathNeedsCode = indivBathroomInfo.needsCode;
+    const bathNeedsKey = indivBathroomInfo.needsKey;
+    const bathHandicapAccess = indivBathroomInfo.handicapAccess;
+    const bathGendered = indivBathroomInfo.gendered;
+    const bathCode = indivBathroomInfo.code;
+    const bathId = indivBathroomInfo.id;
+
     const origins = [Object.values(props.currentLocation).join()];
     const destinations = [Object.values(indivBathroomInfo.longLat).join()];
     const travelMode = props.travelMode.length ? this.state.travelMode : 'WALKING';
@@ -16,18 +25,25 @@ export function fetchDistanceDuration(indivBathroomInfo, props) {
     return fetch(distance.matrix(origins, destinations, travelMode, (err, distances) => {
       let dist = distances.rows[0].elements[0].distance.text;
       let dur = distances.rows[0].elements[0].duration.text;
-      let distDurArray = [dist, dur];
-      console.log(distDurArray);
+      let distDurArray = [dist, dur]; //this is coming thru correctly
     })).then(distDurArray => {
-    dispatch(findDistDur(distDurArray));
+    dispatch(findDistDur(distDurArray, bathName, bathAddress, bathNeedsCode, bathNeedsKey, bathHandicapAccess, bathGendered, bathCode, bathId));
     })
   }
 }
 
-export const findDistDur = distDurArray => {
+export const findDistDur = (distDurArray, bathName, bathAddress, bathNeedsCode, bathNeedsKey, bathHandicapAccess, bathGendered, bathCode, bathId) => {
   return ({
     type: types.FETCH_DISTANCE_DURATIONS,
-    distDurArray
+    distDurArray,
+    bathName,
+    bathAddress,
+    bathNeedsCode,
+    bathNeedsKey,
+    bathHandicapAccess,
+    bathGendered,
+    bathCode,
+    bathId,
   })
 }
 
