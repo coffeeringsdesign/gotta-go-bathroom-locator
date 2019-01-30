79ef524810a65f8a55700cc1f0af08695669d01f
Kendra <kendra@coffeeringsdesign.com>
2019-01-29 21:03:30 -0800

    get the reducer to return an array

--- src/reducers/findIndivDistanceDurationReducer.js
+++ src/reducers/findIndivDistanceDurationReducer.js
@@ -7,22 +7,24 @@ const findIndivDistanceDurationReducer = (state = initialState.bathrooms, action
   console.log(state);
   switch (action.type) {
     case types.DISTANCE_DURATIONS:
-    const newBathroom = ({
-      name: action.bathroom[0],
-      address: action.bathroom[1],
-      distance: action.distDurArray[0],
-      duration: action.distDurArray[1],
-      needsCode: action.bathroom[2],
-      needsKey: action.bathroom[3],
-      handicapAccess: action.bathroom[4],
-      gendered: action.bathroom[5],
-      code: action.bathroom[6],
-      id: action.bathroom[7]
-    })
+    // const newBathroom = ({
+    //
+    // })
 
     return [
         ...state,
-        arr: [...state.bathrooms, newBathroom]
+        {
+          name: action.bathroom[0],
+          address: action.bathroom[1],
+          distance: action.distDurArray[0],
+          duration: action.distDurArray[1],
+          needsCode: action.bathroom[2],
+          needsKey: action.bathroom[3],
+          handicapAccess: action.bathroom[4],
+          gendered: action.bathroom[5],
+          code: action.bathroom[6],
+          id: action.bathroom[7]
+        }
     ]
 
   default:
