491359a2f2e7246afc780b8991232bc6dc451035
Kendra <kendra@coffeeringsdesign.com>
2019-02-02 13:23:20 -0800

    get the new ordered bathroom state slice to show correctly

--- src/reducers/sortNearestBathroomsReducer.js
+++ src/reducers/sortNearestBathroomsReducer.js
@@ -5,39 +5,17 @@ const sortNearestBathroomsReducer = (state = initialState.nearestBathrooms, acti
 
   switch (action.type) {
     case types.REORDER_BATHROOMS:
-
     let newBathroomArray = action.bathrooms;
-    console.log(newBathroomArray); //getting the correct array
 
+    function compare(a, b) {
+      let aDistance = (a.distance.split(' ').shift()) * 1000;
+      // console.log(aDistance);
+      let bDistance = (b.distance.split(' ').shift()) * 1000;
+      // console.log(bDistance);
+      return aDistance - bDistance;
+    }
 
-    let please = newBathroomArray.sort(function(a, b) {
-      // console.log(newReorderedBathroomsStateSlice);
-      return a.distance.split(" ").shift() - b.distance.split(" ").shift();
-    })
-    let trial = ['what', 'the', 'fucking', 'hell'];
-    console.log(trial);
-    console.log(please);
-    // function compare(a, b){
-    //   let aDistance = a.distance.split(" ").shift();
-    //   // console.log(aDistance);
-    //   let bDistance= b.distance.split(" ").shift();
-    //   // console.log(bDistance);
-    //   let comparison = 0;
-    //
-    //   if (aDistance > bDistance) {
-    //     comparison = 1;
-    //   } else if (aDistance < bDistance) {
-    //     comparison = -1;
-    //   }
-    //   console.log(comparison);
-    //   return comparison;
-    // }
-    //
-    // let newReorderedBathroomsStateSlice = newBathroomArray.sort(compare);
-
-    // console.log(newReorderedBathroomsStateSlice);
-
-    return state;
+    return newBathroomArray.sort(compare);
 
   default:
     return state;
