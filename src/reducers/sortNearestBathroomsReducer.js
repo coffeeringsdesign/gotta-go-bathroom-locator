e52d1fde39cb9e2c0b42087e208c919477ab742c
Kendra <kendra@coffeeringsdesign.com>
2019-02-02 11:37:17 -0800

    add compare function, test to see why it isn't reordering the array

--- src/reducers/sortNearestBathroomsReducer.js
+++ src/reducers/sortNearestBathroomsReducer.js
@@ -2,13 +2,29 @@ import constants from './../constants';
 const { initialState, types } = constants;
 
 const sortNearestBathroomsReducer = (state = initialState.nearestBathrooms, action) => {
-  let newReorderedBathroomsStateSlice = [];
 
   switch (action.type) {
     case types.REORDER_BATHROOMS:
-    console.log(state);
 
+    let newBathroomArray = action.bathrooms;
+    console.log(newBathroomArray); //getting the correct array
+    function compare(a, b){
 
+      let aDistance = a.distance;
+      console.log(aDistance);
+      let bDistance= b.distance;
+      let comparison = 0;
+
+      if (aDistance > bDistance) {
+        comparison = 1;
+      } else if (aDistance < bDistance) {
+        comparison = -1;
+      }
+      return comparison;
+    }
+
+    let newReorderedBathroomsStateSlice = newBathroomArray.sort(compare);
+    console.log(newReorderedBathroomsStateSlice);
 
     return newReorderedBathroomsStateSlice;
 
