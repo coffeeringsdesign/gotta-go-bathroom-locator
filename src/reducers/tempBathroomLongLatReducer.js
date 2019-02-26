98a3177bc009f456b357917cfdb995c901ea4337
Kendra <kendra@coffeeringsdesign.com>
2019-02-26 12:29:34 -0800

    get the reducer to return new tempLatLong

--- src/reducers/tempBathroomLongLatReducer.js
+++ src/reducers/tempBathroomLongLatReducer.js
@@ -2,25 +2,16 @@ import constants from './../constants';
 const { initialState, types } = constants;
 
 const tempBathroomLongLatReducer = (state = initialState.tempBathroomLongLat, action) => {
+  let tempNewLongLatStateSlice;
 
   switch (action.type) {
     case types.ADD_NEW_BATHROOM_LAT_LNG:
-    return [
-        ...state,
-        {
-          name: action.bathroom[0],
-          address: action.bathroom[1],
-          longLat: action.bathroom[2],
-          distance: action.distDurArray[0],
-          duration: action.distDurArray[1],
-          needsCode: action.bathroom[3],
-          needsKey: action.bathroom[4],
-          handicapAccess: action.bathroom[5],
-          gendered: action.bathroom[6],
-          code: action.bathroom[7],
-          id: action.bathroom[8]
-        }
-    ]
+    console.log(action);
+    let lat = action.lat;
+    let lng = action.lng;
+    let newLatLongObject = { lat, lng }
+    tempNewLongLatStateSlice = Object.assign({}, newLatLongObject);
+    return tempNewLongLatStateSlice;
 
   default:
     return state;
