92a300c55da6843c0ea82fb54e18875df0c45ee1
Kendra <kendra@coffeeringsdesign.com>
2019-03-07 15:01:16 -0800

    get the form to disappear and a thank you to show when submitted

--- src/components/AddBathroomForm.js
+++ src/components/AddBathroomForm.js
@@ -33,22 +33,15 @@ class AddBathroomForm extends Component {
     this.props.dispatch(fetchNewLongLat(bathroom, this.props.dispatch));
   }
 
-  showThankYou() {
-    let thanks = (
-      <div className="thankYouContainer">
-        <h2>Your Input has been submitted.</h2>
-        <h4>Thank you for your contribution!</h4>
-      </div>
-    );
-    console.log(thanks);
-  }
-
   componentDidUpdate(prevProps){
     const bathroomRef = firebase.database().ref('bathrooms');
     if(prevProps.tempBathroomLongLat !== this.props.tempBathroomLongLat){
       bathroomRef.push(this.props.tempBathroomLongLat);
-      this.showThankYou();
     }
+  }
+
+  refreshPage(){
+    document.location.reload(true);
   }
 
   handleSubmit(e) {
@@ -73,50 +66,59 @@ class AddBathroomForm extends Component {
   render() {
     // console.log(this.state.thanks.props);
     let thanks = null;
-    return (<div className="addBathroomsAll">
-    <div className="addBathroomContainer">
+    return (
+    <div className="addBathroomsAll">
       <Logo />
       <GoToBathroomList />
-      <form className="addBathroomForm" onSubmit={this.handleSubmit}>
-        <h4>Name:</h4>
-        <input id="name" className="newBathroomInputs" type="text" name="name" placeholder="Please enter the establishments name..." value={this.props.name} onChange={this.handleChange}/>
+    {this.props.tempBathroomLongLat === null ?
+      <div className="addBathroomContainer">
 
-        <h4>Address:</h4>
-        <input id="address" className="newBathroomInputs" type="text" name="address" placeholder="Please enter the address including City, State & Zip..." value={this.props.address} onChange={this.handleChange}/>
 
-        <h4>Is the bathroom handicap accessible?</h4>
-        <input id="handicapYes" className="newBathroomRadios" type="radio" name="handicapAccess" value="true" onChange={this.handleChange}/>
-        <label>yes</label>
-        <input id="handicapNo" className="newBathroomRadios" type="radio" name="handicapAccess" value="false" onChange={this.handleChange}/>
-        <label>no</label>
+        <form className="addBathroomForm" onSubmit={this.handleSubmit}>
+          <h4>Name:</h4>
+          <input id="name" className="newBathroomInputs" type="text" name="name" placeholder="Please enter the establishments name..." value={this.props.name} onChange={this.handleChange}/>
 
-        <h4>Are the bathrooms gendered?</h4>
-        <input id="genderedYes" className="newBathroomRadios" type="radio" name="gendered" value="true" onChange={this.handleChange}/>
-        <label>yes</label>
-        <input id="genderedNo" className="newBathroomRadios" type="radio" name="gendered" value="false" onChange={this.handleChange}/>
-        <label>no</label>
+          <h4>Address:</h4>
+          <input id="address" className="newBathroomInputs" type="text" name="address" placeholder="Please enter the address including City, State & Zip..." value={this.props.address} onChange={this.handleChange}/>
 
-        <h4>Is a key required for access?</h4>
-        <input id="keyTrue" className="newBathroomRadios" type="radio" name="needsKey" value="true" onChange={this.handleChange}/>
-        <label>yes</label>
-        <input id="keyFalse" className="newBathroomRadios" type="radio" name="needsKey" value="false" onChange={this.handleChange}/>
-        <label>no</label>
+          <h4>Is the bathroom handicap accessible?</h4>
+          <input id="handicapYes" className="newBathroomRadios" type="radio" name="handicapAccess" value="true" onChange={this.handleChange}/>
+          <label>yes</label>
+          <input id="handicapNo" className="newBathroomRadios" type="radio" name="handicapAccess" value="false" onChange={this.handleChange}/>
+          <label>no</label>
 
-        <h4>Is a code required for access?</h4>
-        <input id="codeTrue" className="newBathroomRadios" type="radio" name="needsCode" value="true" onChange={this.handleChange}/>
-        <label>yes</label>
-        <input id="codeFalse" className="newBathroomRadios" type="radio" name="needsCode" value="false" onChange={this.handleChange}/>
-        <label>no</label>
+          <h4>Are the bathrooms gendered?</h4>
+          <input id="genderedYes" className="newBathroomRadios" type="radio" name="gendered" value="true" onChange={this.handleChange}/>
+          <label>yes</label>
+          <input id="genderedNo" className="newBathroomRadios" type="radio" name="gendered" value="false" onChange={this.handleChange}/>
+          <label>no</label>
 
-        <h4>Do you know the code? If so, please enter it here:</h4>
-        <input id="code" className="newBathroomInputs" type="text" name="code" placeholder="leave blank if no code is needed..." value={this.props.code} onChange={this.handleChange}/>
+          <h4>Is a key required for access?</h4>
+          <input id="keyTrue" className="newBathroomRadios" type="radio" name="needsKey" value="true" onChange={this.handleChange}/>
+          <label>yes</label>
+          <input id="keyFalse" className="newBathroomRadios" type="radio" name="needsKey" value="false" onChange={this.handleChange}/>
+          <label>no</label>
 
-        <br></br>
-        <button className="searchButton" type="submit">Add Bathroom to Database</button>
-      </form>
-    </div>
-    {thanks}
-</div>)
+          <h4>Is a code required for access?</h4>
+          <input id="codeTrue" className="newBathroomRadios" type="radio" name="needsCode" value="true" onChange={this.handleChange}/>
+          <label>yes</label>
+          <input id="codeFalse" className="newBathroomRadios" type="radio" name="needsCode" value="false" onChange={this.handleChange}/>
+          <label>no</label>
+
+          <h4>Do you know the code? If so, please enter it here:</h4>
+          <input id="code" className="newBathroomInputs" type="text" name="code" placeholder="leave blank if no code is needed..." value={this.props.code} onChange={this.handleChange}/>
+
+          <br></br>
+          <button className="searchButton" type="submit">Add Bathroom to Database</button>
+        </form>
+      </div> : null}
+    {this.props.tempBathroomLongLat !== null ?
+      <div className="thankYouContainer">
+      <h2>Your Input has been submitted.</h2>
+      <h4>Thank you for your contribution!</h4>
+      <button className="goToAddaBathroom" type="button" onClick="this.refreshPage()">Add Another?</button>
+      </div> : null}
+    </div>)
   }
 };
 
